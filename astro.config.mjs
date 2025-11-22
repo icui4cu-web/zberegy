6fcdc870af77725a7b9c331127dc487e696a0388
Александр <icui4cu.web@gmail.com>
2025-11-22 17:50:37 +0200

    beautifier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import htmlBeautifier from "astro-html-beautifier";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	compressHTML: false,
+	trailingSlash: 'never',
+	build: {
+		inlineStylesheets: 'never',
+		format: 'preserve',
+		assets: 'assets'
+	},
+	integrations: import.meta.env.PROD ? [
+		htmlBeautifier({ inline: [] })
+	] : []
+});
